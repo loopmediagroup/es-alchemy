37bc23cb16dc6726c2c7dc07f4e78c6eed2d4fe2
Lukas Siemon <lukas.siemon@getintheloop.ca>
2018-08-25 16:18:21 -0700

    chore: added deps fix: .flowconfig fix: minor eslint

--- src/util/load-json-in-dir.js
+++ src/util/load-json-in-dir.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 // load all json files in directory
 // returns `{ [FILENAME_WITHOUT_EXT]: FILE_CONTENT }`
-module.exports = (folder) => fs
+module.exports = folder => fs
   .readdirSync(folder)
   .filter(f => f.endsWith(".json"))
   .map(f => f.slice(0, -5))
