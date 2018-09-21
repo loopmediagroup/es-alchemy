25b13f619b0f151899f68bcdb120dcacd0aa445f
Lukas Siemon <lukas.siemon@getintheloop.ca>
2018-09-21 11:47:02 -0700

    fix: now allowing array field types (no difference in es) + assert

--- src/util/model.js
+++ src/util/model.js
@@ -8,10 +8,16 @@ module.exports = ({
       typeof specs.fields === "object" && !Array.isArray(specs.fields),
       "Model definition expected to be of type object."
     );
+    assert(
+      Object.values(specs.fields).every(f => fieldDefinitions[f.endsWith("[]") ? f.slice(0, -2) : f] !== undefined),
+      "Unknown field type given."
+    );
     return Object.assign({}, specs, {
       fields: Object
         .entries(specs.fields)
-        .reduce((prev, [key, value]) => Object.assign(prev, { [key]: fieldDefinitions[value] }), {})
+        .reduce((prev, [key, value]) => Object.assign(prev, {
+          [key]: fieldDefinitions[value.endsWith("[]") ? value.slice(0, -2) : value]
+        }), {})
     });
   }
 });
