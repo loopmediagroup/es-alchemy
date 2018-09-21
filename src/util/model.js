4b69ad0fa4e530d8ea63a27791be23ce31145585
Lukas Siemon <lukas.siemon@getintheloop.ca>
2018-09-21 11:56:35 -0700

    amend: improved debug

--- src/util/model.js
+++ src/util/model.js
@@ -8,10 +8,10 @@ module.exports = ({
       typeof specs.fields === "object" && !Array.isArray(specs.fields),
       "Model definition expected to be of type object."
     );
-    assert(
-      Object.values(specs.fields).every(f => fieldDefinitions[f.endsWith("[]") ? f.slice(0, -2) : f] !== undefined),
-      "Unknown field type given."
-    );
+    Object.values(specs.fields).forEach(fieldType => assert(
+      fieldDefinitions[fieldType.endsWith("[]") ? fieldType.slice(0, -2) : fieldType] !== undefined,
+      `Unknown field type given: ${fieldType}`
+    ));
     return Object.assign({}, specs, {
       fields: Object
         .entries(specs.fields)
