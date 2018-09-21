ce90bf425c7af12f5c7bb85a9822c484cff6fa99
Lukas Siemon <lukas.siemon@getintheloop.ca>
2018-09-21 07:55:38 -0700

    amend: restruct

--- src/util/get-parents.js
+++ src/util/get-parents.js
@@ -3,10 +3,9 @@ module.exports = fields => Array.from(fields.reduce((p, field) => {
     .split(".")
     .slice(0, -1)
     .reduce((prev, next) => {
-      prev.push((prev[prev.length - 1] || []).concat([next]));
+      prev.push(prev.length === 0 ? next : `${prev[prev.length - 1]}.${next}`);
       return prev;
     }, [])
-    .map(e => e.join("."))
     .forEach(e => p.add(e));
   return p;
 }, new Set()));
