208c9a4ec6b559f33a2d8e1e2ad269d3e85f1e20
Lukas Siemon <github@blackflux.com>
2019-04-25 16:37:42 -0700

    fix: now magically injecting empty arrays

--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@ module.exports = (options) => {
     query: {
       build: (idx = null, opts = {}) => query.build(idx === null ? null : indices[idx].fields, opts)
     },
-    rest: rest(idx => get(indices[idx], 'mapping', null), options)
+    rest: rest(idx => get(indices[idx], 'specs', null), idx => get(indices[idx], 'mapping', null), options)
   };
 };
 
