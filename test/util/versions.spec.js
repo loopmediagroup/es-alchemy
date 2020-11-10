6b6e8dc3add2678b0948032119f64e26c38315c7
Kodi Ackerman <kodiackerman@msn.com>
2020-11-09 16:15:54 -0800

    amend: fixtures

--- test/util/versions.spec.js
+++ test/util/versions.spec.js
@@ -1,5 +1,4 @@
 const { expect } = require('chai');
-const { v4: uuid4 } = require('uuid');
 const { describe } = require('node-tdd');
 const Joi = require('joi-strict');
 const Versions = require('../../src/util/versions');
@@ -8,34 +7,34 @@ describe('Testing Versions', {
   useTmpDir: true
 }, () => {
   let versions;
-  let hash;
-  let indices;
 
   beforeEach(() => {
     versions = Versions();
-    hash = uuid4();
-    indices = { offer: { mapping: { mappings: { _meta: { hash } } } } };
   });
 
-  it('Testing list', ({ dir }) => {
-    expect(versions.persist(indices, dir)).to.equal(true);
+  it('Testing list', ({ fixture, dir }) => {
+    const offerIndex = fixture('offer');
+    expect(versions.persist(offerIndex, dir)).to.equal(true);
     versions.load(dir);
     const result = versions.list();
     expect(result).to.deep.equal(['offer']);
   });
 
-  it('Testing get', ({ dir }) => {
-    expect(versions.persist(indices, dir)).to.equal(true);
+  it('Testing get', ({ fixture, dir }) => {
+    const offerIndex = fixture('offer');
+    expect(versions.persist(offerIndex, dir)).to.equal(true);
     versions.load(dir);
     const result = versions.get('offer');
     const schema = Joi.object().pattern(
-      Joi.string().valid(hash),
+      Joi.string().valid('6a1b8f491e156e356ab57e8df046b9f449acb440'),
       Joi.object().keys({
         timestamp: Joi.number().integer(),
-        mapping: Joi.object()
+        specs: Joi.object(),
+        mapping: Joi.object(),
+        fields: Joi.array().items(Joi.string()),
+        rels: Joi.object()
       })
     );
-    expect(Object.keys(result).sort()).to.deep.equal([hash]);
     expect(Joi.test(result, schema)).to.equal(true);
   });
 });
