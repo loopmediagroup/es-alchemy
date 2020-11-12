27ecea73d7699f9df1eaa9854c9d1ac7becee04d
Kodi Ackerman <kodiackerman@msn.com>
2020-11-12 14:15:48 -0800

    amend: refactoring. Using sync

--- src/util/rest/mapping/sync.js
+++ src/util/rest/mapping/sync.js
@@ -1,28 +1,14 @@
 const assert = require('assert');
-
-const getIndices = async (call, idx) => {
-  const result = await call('GET', '', {
-    endpoint: `_cat/indices/${idx}@*`
-  });
-  return result.body.map(({ index }) => index.split('@')[1]);
-};
-
-const createIndex = (call, idx, mapping) => call(
-  'PUT',
-  // eslint-disable-next-line no-underscore-dangle
-  `${idx}@${mapping.mappings._meta.hash}`,
-  { body: mapping }
-).then((r) => r.statusCode === 200 && r.body.acknowledged === true);
+const { getIndexVersions, createIndexVersion } = require('../index/versions');
 
 module.exports = async (call, versions, idx) => {
-  const versionsByIndex = versions.get(idx);
-  assert(versionsByIndex !== undefined, 'Index must be loaded');
-  const indexVersions = await getIndices(call, idx);
-  const indicesToCreate = Object.entries(versionsByIndex).filter(([key, _]) => !indexVersions.includes(key));
-  const indicesCreatedResult = await Promise.all(
-    indicesToCreate.map(([_, { mapping }]) => createIndex(call, idx, mapping))
+  const localVersions = versions.get(idx);
+  const remoteVersions = await getIndexVersions(call, idx);
+  const versionsToCreate = Object.entries(localVersions).filter(([key, _]) => !remoteVersions.includes(key));
+  const versionsCreatedResult = await Promise.all(
+    versionsToCreate.map(([_, { mapping }]) => createIndexVersion(call, idx, mapping))
   );
-  const indicesCreated = indicesToCreate.map(([v, _]) => `${idx}@${v}`);
-  assert(indicesCreatedResult.every((e) => e === true), indicesCreated);
-  return indicesCreated;
+  const versionsCreated = versionsToCreate.map(([v, _]) => `${idx}@${v}`);
+  assert(versionsCreatedResult.every((e) => e === true), versionsCreated);
+  return versionsCreated;
 };
