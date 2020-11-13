a6da78a086944c753b3cfca30f3282340f90f6c0
Kodi Ackerman <kodiackerman@msn.com>
2020-11-13 11:35:15 -0800

    amend: tests and refactoring

--- src/util/rest/mapping/diverged.js
+++ src/util/rest/mapping/diverged.js
@@ -1,10 +1,5 @@
 const traverse = require('../../../misc/traverse');
 
-const getPersistedVersionsByIndex = (versions, idx) => {
-  const persistedVersions = Object.keys(versions.get(idx));
-  return persistedVersions.map((version) => `${idx}@${version}`);
-};
-
 const listDocuments = async (call, idx, cursor) => {
   const result = await call('GET', idx, {
     endpoint: '_search',
@@ -12,20 +7,15 @@ const listDocuments = async (call, idx, cursor) => {
       _source: ['id'],
       ...(cursor === null ? {} : { search_after: [cursor] }),
       size: 99,
-      sort: [
-        {
-          _id: {
-            order: 'asc'
-          }
-        }
-      ]
+      sort: [{ _id: { order: 'asc' } }]
     }
   });
   return result.body.hits.hits.map(({ _id: id }) => id);
 };
 
 module.exports = async (call, versions, mapping, idx, cursor = null) => {
-  const localVersions = getPersistedVersionsByIndex(versions, idx);
+  const localVersions = Object.keys(versions.get(idx))
+    .map((version) => `${idx}@${version}`);
   if (cursor !== null) {
     const cursorKeys = Object.keys(cursor);
     if (cursorKeys.length !== localVersions.length || !cursorKeys.every((c) => localVersions.includes(c))) {
--- test/util/rest/mapping/diverged.spec.js
+++ test/util/rest/mapping/diverged.spec.js
@@ -1,6 +1,9 @@
+const path = require('path');
 const { expect } = require('chai');
-const { describe } = require('node-tdd');
 const { v4: uuid4 } = require('uuid');
+const Joi = require('joi-strict');
+const sfs = require('smart-fs');
+const { describe } = require('node-tdd');
 const Index = require('../../../../src/index');
 const { registerEntitiesForIndex } = require('../../../helper');
 
@@ -9,15 +12,30 @@ describe('Testing diverged', {
   cryptoSeed: 'd9b0ed0b-a3d9-4cd6-85d8-0d090ad4bf3b'
 }, () => {
   let index;
+  let instantiateIndex;
+  let updatedOfferModel;
+  let updatedOfferIndex;
   let offerId;
 
+  before(() => {
+    instantiateIndex = () => {
+      index = Index({ endpoint: process.env.elasticsearchEndpoint });
+      registerEntitiesForIndex(index);
+    };
+    const [offerModelPath, offerIndexPath] = ['models', 'indices']
+      .map((v) => path.join(__dirname, '..', '..', '..', `${v}`, 'offer.json'));
+    updatedOfferModel = sfs.smartRead(offerModelPath);
+    updatedOfferIndex = sfs.smartRead(offerIndexPath);
+    updatedOfferModel.fields.subhead = 'string';
+    updatedOfferIndex.fields.push('subhead');
+  });
+
   beforeEach(() => {
-    index = Index({ endpoint: process.env.elasticsearchEndpoint });
-    registerEntitiesForIndex(index);
+    instantiateIndex();
     offerId = uuid4();
   });
 
-  it('Testing diverged with one index', async ({ dir }) => {
+  it('Testing single index', async ({ dir }) => {
     expect(await index.rest.mapping.create('offer')).to.equal(true);
     expect(index.index.versions.persist(dir)).to.equal(true);
     expect(index.index.versions.load(dir)).to.equal(undefined);
@@ -38,7 +56,7 @@ describe('Testing diverged', {
     expect(await index.rest.mapping.delete('offer')).to.equal(true);
   });
 
-  it('Testing diverged with one index and a cursor', async ({ dir }) => {
+  it('Testing index with cursor', async ({ dir }) => {
     expect(await index.rest.mapping.create('offer')).to.equal(true);
     expect(index.index.versions.persist(dir)).to.equal(true);
     expect(index.index.versions.load(dir)).to.equal(undefined);
@@ -66,7 +84,7 @@ describe('Testing diverged', {
     expect(await index.rest.mapping.delete('offer')).to.equal(true);
   });
 
-  it('Testing diverged with invalid cursor keys', async ({ dir, capture }) => {
+  it('Testing invalid cursor keys', async ({ dir, capture }) => {
     expect(await index.rest.mapping.create('offer')).to.equal(true);
     expect(index.index.versions.persist(dir)).to.equal(true);
     expect(index.index.versions.load(dir)).to.equal(undefined);
@@ -78,6 +96,46 @@ describe('Testing diverged', {
       }
     }));
     expect(error.message).to.equal('Invalid cursor keys');
+    expect(await index.rest.mapping.delete('offer')).to.equal(true);
+  });
+
+  it('Testing documents not in sync', async ({ dir, capture }) => {
+    expect(await index.rest.mapping.create('offer')).to.equal(true);
+    expect(index.index.versions.persist(dir)).to.equal(true);
+    expect(index.index.versions.load(dir)).to.equal(undefined);
+    expect(await index.rest.data.update('offer', [{
+      action: 'update',
+      doc: index.data.remap('offer', {
+        id: offerId,
+        headline: 'headline'
+      })
+    }])).to.equal(true);
+    expect(await index.rest.data.refresh('offer')).to.equal(true);
+    instantiateIndex();
+    index.model.register('offer', updatedOfferModel);
+    index.index.register('offer', updatedOfferIndex);
+    expect(await index.rest.mapping.create('offer')).to.equal(true);
+    expect(index.index.versions.persist(dir)).to.equal(true);
+    expect(index.index.versions.load(dir)).to.equal(undefined);
+    const offerId2 = uuid4();
+    expect(await index.rest.data.update('offer', [{
+      action: 'update',
+      doc: index.data.remap('offer', {
+        id: offerId2,
+        headline: 'headline',
+        subhead: 'subhead'
+      })
+    }])).to.equal(true);
+    expect(await index.rest.data.refresh('offer')).to.equal(true);
+    const schema = Joi.object().keys({ // TODO: update this
+      result: Joi.array().items(Joi.string().guid()),
+      cursor: Joi.object().keys({
+        'offer@e35ec51a3c35e2d9982e1ac2bbe23957a637a9e0': Joi.string().guid(),
+        'offer@6a1b8f491e156e356ab57e8df046b9f449acb440': Joi.string().guid()
+      })
+    });
+    const result = await index.rest.mapping.diverged('offer');
+    expect(Joi.test(result, schema)).to.equal(true);
     expect(await index.rest.mapping.delete('offer')).to.equal(true);
   });
 });
