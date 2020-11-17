81b88076cea4b67bc8503f342324e0c6f662e0e3
Kodi Ackerman <kodiackerman@msn.com>
2020-11-17 08:50:35 -0800

    amend: countResult check

--- src/util/rest/data/synced.js
+++ src/util/rest/data/synced.js
@@ -3,5 +3,8 @@ const count = require('./count');
 module.exports = async (call, versions, idx) => {
   const localVersions = Object.keys(versions.get(idx));
   const countResult = await Promise.all(localVersions.map((version) => count(call, `${idx}@${version}`)));
+  if (countResult.some((c) => !Number.isInteger(c))) {
+    return false;
+  }
   return countResult.every((c, _, arr) => c === arr[0]);
 };
