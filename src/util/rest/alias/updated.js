4c83be550973ff6475a3292ab3f8b5c1cc22786d
Lukas Siemon <github@blackflux.com>
2020-11-17 14:51:27 -0800

    chore: refactor alias.updated

--- src/util/rest/alias/updated.js
+++ src/util/rest/alias/updated.js
@@ -1,12 +1,6 @@
-const assert = require('assert');
-
 module.exports = async (call, idx, mapping) => {
-  const result = (await call('GET', '', {
-    endpoint: `_cat/aliases/${idx}`
-  })).body;
-  assert([0, 1].includes(result.length), result);
-  return result.length === 0
-    ? false
-    // eslint-disable-next-line no-underscore-dangle
-    : `${idx}@${mapping.mappings._meta.hash}` === result[0].index;
+  // eslint-disable-next-line no-underscore-dangle
+  const version = `${idx}@${mapping.mappings._meta.hash}`;
+  const r = await call('GET', '', { endpoint: `${version}/_alias/${idx}` });
+  return r.statusCode === 200 && Object.keys(r.body[version].aliases[idx]).length === 0;
 };
