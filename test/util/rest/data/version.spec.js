12a4e15bd243aeda7d17c9c3877baa9d94041ab4
Lukas Siemon <github@blackflux.com>
2020-11-16 17:21:24 -0800

    chore: refactor

--- test/util/rest/data/version.spec.js
+++ test/util/rest/data/version.spec.js
@@ -6,18 +6,23 @@ const { registerEntitiesForIndex } = require('../../../helper');
 
 describe('Testing version', { useTmpDir: true }, () => {
   let index;
+  let offerId;
 
   beforeEach(async ({ dir }) => {
     index = Index({ endpoint: process.env.elasticsearchEndpoint });
     registerEntitiesForIndex(index);
     expect(await index.index.versions.persist(dir)).to.equal(true);
     expect(await index.index.versions.load(dir)).to.equal(undefined);
+    expect(await index.rest.mapping.create('offer')).to.equal(true);
+    expect(await index.rest.alias.update('offer')).to.equal(true);
+    offerId = uuid4();
+  });
+
+  afterEach(async () => {
+    expect(await index.rest.mapping.delete('offer')).to.equal(true);
   });
 
   it('Test retrieving a version number for a document', async () => {
-    const offerId = uuid4();
-    expect(await index.rest.mapping.create('offer')).to.equal(true);
-    expect(await index.rest.alias.update('offer')).to.equal(true);
     expect(await index.rest.data.update('offer', [{
       action: 'update',
       doc: index.data.remap('offer', {
@@ -36,13 +41,9 @@ describe('Testing version', { useTmpDir: true }, () => {
     }])).to.equal(true);
     expect(await index.rest.data.refresh('offer')).to.equal(true);
     expect(await index.rest.data.version('offer', offerId)).to.equal(2);
-    expect(await index.rest.mapping.delete('offer')).to.equal(true);
   });
 
   it('Test version and signature do not increase if update is identical', async () => {
-    const offerId = uuid4();
-    expect(await index.rest.mapping.create('offer')).to.equal(true);
-    expect(await index.rest.alias.update('offer')).to.equal(true);
     expect(await index.rest.data.update('offer', [{
       action: 'update',
       doc: index.data.remap('offer', {
@@ -63,13 +64,9 @@ describe('Testing version', { useTmpDir: true }, () => {
     expect(await index.rest.data.refresh('offer')).to.equal(true);
     expect(await index.rest.data.version('offer', offerId)).to.equal(1);
     expect(await index.rest.data.signature('offer', offerId)).to.equal('0_1');
-    expect(await index.rest.mapping.delete('offer')).to.equal(true);
   });
 
   it('Test version and signature do not increase if update is identical (with signature)', async () => {
-    const offerId = uuid4();
-    expect(await index.rest.mapping.create('offer')).to.equal(true);
-    expect(await index.rest.alias.update('offer')).to.equal(true);
     expect(await index.rest.data.update('offer', [{
       action: 'update',
       doc: index.data.remap('offer', {
@@ -92,18 +89,14 @@ describe('Testing version', { useTmpDir: true }, () => {
     expect(await index.rest.data.refresh('offer')).to.equal(true);
     expect(await index.rest.data.version('offer', offerId)).to.equal(1);
     expect(await index.rest.data.signature('offer', offerId)).to.equal('0_1');
-    expect(await index.rest.mapping.delete('offer')).to.equal(true);
   });
 
   it('Test throws an error if index does not exist', async ({ capture }) => {
-    const error = await capture(() => index.rest.data.version('offer', 'id'));
+    const error = await capture(() => index.rest.data.version('unknown', 'id'));
     expect(error.message).to.equal('index_not_found_exception');
   });
 
   it('Test returning null if document does not exist', async () => {
-    expect(await index.rest.mapping.create('offer')).to.equal(true);
-    expect(await index.rest.alias.update('offer')).to.equal(true);
     expect(await index.rest.data.version('offer', 'id')).to.equal(null);
-    expect(await index.rest.mapping.delete('offer')).to.equal(true);
   });
 });
