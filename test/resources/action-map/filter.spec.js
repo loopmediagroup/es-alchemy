80291659093a3bfd55d306bc54cb168a1cf65ba4
Ezequiel Alanis <ealanisg@gmail.com>
2020-12-04 14:36:17 -0800

    amend: move helper logic to test

--- test/resources/action-map/filter.spec.js
+++ test/resources/action-map/filter.spec.js
@@ -1,6 +1,13 @@
+const get = require('lodash.get');
 const expect = require('chai').expect;
 const { describe } = require('node-tdd');
-const fn = require('./helper');
+const { search } = require('../../../src/resources/action-map/filter');
+
+const fn = (q) => {
+  const result = search('name', q);
+  const ws = get(result, 'bool.filter');
+  return ws.map((w) => get(w, 'wildcard.name.value'));
+};
 
 describe('Testing search filter', () => {
   it('Testing match', () => {
--- test/resources/action-map/helper.js
+++
@@ -1,8 +0,0 @@
-const get = require('lodash.get');
-const { search } = require('../../../src/resources/action-map/filter');
-
-module.exports = (q) => {
-  const result = search('name', q);
-  const ws = get(result, 'bool.filter');
-  return ws.map((w) => get(w, 'wildcard.name.value'));
-};
