172ba827b9f35bd4b2ee766628886b77c2707baa
Ezequiel Alanis <ealanisg@gmail.com>
2020-12-04 15:12:53 -0800

    amend: minor

--- test/resources/action-map/filter.spec.js
+++ test/resources/action-map/filter.spec.js
@@ -3,7 +3,7 @@ const expect = require('chai').expect;
 const { describe } = require('node-tdd');
 const { search } = require('../../../src/resources/action-map/filter');
 
-const fn = (q) => {
+const normalize = (q) => {
   const result = search('name', q);
   const ws = get(result, 'bool.filter');
   return ws.map((w) => get(w, 'wildcard.name.value'));
@@ -11,12 +11,7 @@ const fn = (q) => {
 
 describe('Testing search filter', () => {
   it('Testing match', () => {
-    expect(fn('Crème Brulée garçon niÑo'))
-      .to.deep.equal([
-        'Crème*',
-        'Brulée*',
-        'garçon*',
-        'niÑo*'
-      ]);
+    expect(normalize('Crème Brulée garçon niÑo'))
+      .to.deep.equal(['Crème*', 'Brulée*', 'garçon*', 'niÑo*']);
   });
 });
